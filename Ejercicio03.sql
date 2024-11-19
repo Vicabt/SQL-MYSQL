71f4b91896d1703bc7e3263755aef984faab8721
Vicabt <vicab66@gmail.com>
2024-11-18 21:00:11 -0500

    Delete Ejercicio03.sql

--- Ejercicio03.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS db_inventario;
-USE db_inventario;
-
-CREATE TABLE proveedores (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nombre VARCHAR(50) NOT NULL,
-    contacto VARCHAR(50),
-    telefono VARCHAR(15)
-);
