196a00281cc5ec3c72adf15e04799b8e2a7c70a4
Vicabt <vicab66@gmail.com>
2024-11-18 21:00:04 -0500

    Delete Ejercicio02.sql

--- Ejercicio02.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS db_tienda;
-USE db_tienda;
-
-CREATE TABLE empleados (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nombre VARCHAR(50) NOT NULL,
-    puesto VARCHAR(50) NOT NULL,
-    salario DECIMAL(10, 2) NOT NULL
-);
